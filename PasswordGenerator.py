5822acdcab42fea16471308395e570bd3cf1f24d
AndreTreto <84688092+AndreTreto@users.noreply.github.com>
2024-03-08 07:56:10 -0500

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,21 +1,25 @@
-from itertools import permutations
+from itertools import product
 
-f = open("passwords.txt", "w")
+f = open("passwords.txt", "w") #opens file for truncating
 
-f.truncate()
+f.truncate() #deletes anything previously stored in file
 
+#gather information from user
 min_length = int(input("Minimum length of the passwords? :"))
-amount = int(input("How many strings do you want per password? :"))
+amount = input("How many strings do you want per password? :")
 strings = input("Enter info with commas in between:")
 stringlist = strings.split(',')
 
-permutations = list(permutations(stringlist, amount))
+#calculate cartesian product using information provided
+product = list(product(stringlist, repeat=int(amount)))
 
-f = open("passwords.txt", "a")
+f = open("passwords.txt", "a") #open file for appending
 
-for i in permutations:
-  if len(i) >= min_length:
-    print(''.join(i))
-    f.write(''.join(i) + '\n')
-    
+#iterate through product list and join the elemts together to form a string
+for i in product: 
+    password = str(''.join(i)) 
+    if len(password) >= min_length: #check length of string
+      print(password)
+      f.write(password + '\n') #write to file
+
 f.close()
